81e9cf8867416064c4e77278aa59eefa81b497d5
Radu <radu.groza@bistone.eu>
2021-07-13 15:32:44 +0300

    action field type

--- automator-examples.php
+++ automator-examples.php
@@ -27,10 +27,3 @@ add_action( 'thrive_automator_init', static function () {
 	/* end "Webhook" action registration */
 
 } );
-
-add_action( 'wp_enqueue_scripts', static function () {
-	$user = get_user_by( 'id', 1 );
-
-	do_action( 'wp_login', $user->user_login, $user );
-	die('triggered action');
-} );
